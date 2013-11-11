3ef523d1d1bb8a7b395331505d5efbe13c99675d
Margots Kapacs <makapacs@hotmail.com>
2013-11-11 17:49:24 -0600

    Hotfix-2.0.2: removed dependency so subthemes can be cloned,fix img for some bootswatches,  added a test sub theme test_subtheme

--- sites/all/themes/bootstrap/theme-settings.php
+++ sites/all/themes/bootstrap/theme-settings.php
@@ -58,5 +58,49 @@ function bootstrap_form_system_theme_settings_alter(&$form, $form_state, $form_i
     '#default_value' => theme_get_setting('cdn_bootstrap_version'),
   );
 
+    $form['bootswatch'] = array(
+        '#title' => 'Bootswatch Configurations - Kapasoft.com',
+        '#type' => 'fieldset',
+        '#collapsible' => FALSE,
+        '#collapsed' => FALSE,
+    );
+
+    $form['bootswatch']['bootswatch_theme'] = array(
+        '#title' => t('Bootswatch Themes'),
+        '#type' => 'select',
+        '#default_value' => theme_get_setting('bootswatch_theme'),
+        '#options' => _theme_schemas(),
+    );
+
+}
+//function dave_subtheme_form_system_theme_settings_alter(&$form, &$form_state){
+//
+//
+//    $form['bootswatch'] = array(
+//        '#title' => 'Bootswatch Configurations - Kapasoft.com',
+//        '#type' => 'fieldset',
+//        '#collapsible' => FALSE,
+//        '#collapsed' => FALSE,
+//    );
+//
+//    $form['bootswatch']['bootswatch_theme'] = array(
+//        '#title' => t('Bootswatch Themes'),
+//        '#type' => 'select',
+//        '#default_value' => theme_get_setting('bootswatch_theme'),
+//        '#options' => _theme_schemas(),
+//    );
+//}
+
+function _theme_schemas(){
+    $themes = array();
+    $active_theme_path = drupal_get_path('theme', $GLOBALS['theme_key']);
+    $themeNames = scandir('./'. $active_theme_path.'/css/bootswatch');
+
+    foreach($themeNames as $dir){
+        if($dir != '.' && $dir != '..' && $dir != 'fonts'){
+            $themes[$dir] = $dir;
+        }
+    }
+    return $themes;
 }
 
